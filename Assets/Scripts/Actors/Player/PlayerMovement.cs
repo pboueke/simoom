9be5a56686c348ed614a4aea8e229c83d872d3de
mseefelder <saa.marcos@gmail.com>
2017-03-21 21:11:58 -0300

    Added first developments for minimum playable version.

---
+++ Assets/Scripts/Actors/Player/PlayerMovement.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float speed = 6f;
+	Vector3 movement;
+	Animator anim;
+	Rigidbody playerRigidbody;
+	int floorMask;
+	float camRayLength = 100f;
+
+	// Gets called regardless of wether the script is enabled or not
+	void Awake () {
+		floorMask = LayerMask.GetMask("Floor");
+		anim = GetComponent <Animator> ();
+		playerRigidbody = GetComponent <Rigidbody> ();
+	}
+
+	// Function which Unity will automatically call on its scripts
+	//whenever there's a physics update
+	void FixedUpdate () {
+		float h = Input.GetAxisRaw("Horizontal");
+		float v = Input.GetAxisRaw("Vertical");
+		
+		Move(h,v);
+		Turning();
+		Animating(h,v);
+	}
+
+	void Move (float h, float v) {
+		movement.Set(h, 0f, v);
+		movement = movement.normalized * speed * Time.deltaTime;
+		playerRigidbody.MovePosition(transform.position + movement);
+	}
+
+	void Turning () {
+		Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit floorHit;
+		if (Physics.Raycast(camRay, out floorHit, camRayLength, floorMask)) {
+			Vector3 playerToMouse = floorHit.point - transform.position;
+			playerToMouse.y = 0f;
+
+			Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
+			playerRigidbody.MoveRotation(newRotation);
+		}
+	}
+
+	void Animating (float h, float v) {
+		bool running = h!=0f || v!=0f;
+		anim.SetBool("isRunning", running);
+	}
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
