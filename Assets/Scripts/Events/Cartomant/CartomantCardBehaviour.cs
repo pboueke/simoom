2a3d2d17e178f08558a39ff57e1bb1995e996a0e
Pedro Boueke <pedro.boueke@bigdatacorp.com.br>
2017-04-05 18:05:33 -0300

    Cartomant event prefab created.

---
+++ Assets/Scripts/Events/Cartomant/CartomantCardBehaviour.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CartomantCardBehaviour : MonoBehaviour {
+
+    public string playerEventColliderName = "PlayerEventCollider";
+
+    private bool inTrigger = false;
+
+    private Renderer rend;
+    private CartomantDeckBehaviour deck;
+
+    void Start () {
+        rend = GetComponent<Renderer>();
+        deck = transform.parent.gameObject.GetComponent<CartomantDeckBehaviour>();
+    }
+	
+	void Update () {
+        if (inTrigger && Input.GetKeyDown(KeyCode.E))
+        {
+            // activate the event
+            deck.applyEffect();
+        }
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        inTrigger = true;
+
+        if (deck.yetToBeUsed && col.gameObject.name == playerEventColliderName)
+        {
+            showHighlight();
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        inTrigger = false;
+
+        if (deck.yetToBeUsed && col.gameObject.name == playerEventColliderName)
+        {
+            hideHighLight();
+        }
+    }
+
+    // highlight funcions
+    private void showHighlight()
+    {
+        // shader defined at /assets/shaders/$self_illuminated_outlined_diffuse_113.shader
+        rend.material.shader = Shader.Find("Self-Illumin/Outlined Diffuse");
+    }
+
+    private void hideHighLight()
+    {
+        rend.material.shader = Shader.Find("Diffuse");
+    }
+}
---
+++ Assets/Scripts/Events/Cartomant/CartomantDeckBehaviour.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CartomantDeckBehaviour : MonoBehaviour {
+
+    public bool yetToBeUsed = true;
+
+    private GameObject _player;
+    private PlayerHealth _playerHealth;
+
+    // effect tunnig
+    private ParticleSystem Effect0_Explosion;
+    public float Effectt0_ExplosionDamage = 10.0f;
+
+    // Use this for initialization
+    void Start () {
+        // general init
+        _player = GameObject.FindGameObjectWithTag("Player");
+        _playerHealth = _player.GetComponent<PlayerHealth>();
+
+        // effects
+        Effect0_Explosion = transform.GetComponentInChildren<ParticleSystem>();
+
+    }
+
+    void Update() { }
+
+    public void applyEffect()
+    {
+        // choose our *random* effect
+        int eventId = 0; // set random id
+
+        switch (eventId)
+        {
+            case 0:
+                applyPlayerExplosion();
+                break;
+        }
+
+        yetToBeUsed = false;
+    }
+
+    // effect functions
+
+    // Effect Id: 0
+    // Description: small explosion on player dealing some damage
+    private void applyPlayerExplosion()
+    {
+        _playerHealth.TakeDamage(Effectt0_ExplosionDamage);
+        Effect0_Explosion.Play();
+        Destroy(Effect0_Explosion.gameObject, Effect0_Explosion.main.duration);
+    }
+}
