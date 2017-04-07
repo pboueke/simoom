5400a01da99b6aafcff3a5a2614aef8e4e2b44d1
mseefelder <saa.marcos@gmail.com>
2017-04-07 08:48:42 -0300

    Added simple health bar to player.

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -1,12 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour {
 
     public float _startingHealth = 100f;
     private float _currentHealth;
     private bool _dead;
+    public Slider _slider;                             // The slider to represent how much health the tank currently has.
+    public Image _fillImage;                           // The image component of the slider.
+    public Color _fullHealthColor = Color.green;       // The color the health bar will be when on full health.
+    public Color _zeroHealthColor = Color.red;         // The color the health bar will be when on no health.
+        
 
     // Use this for initialization
     private void Awake()
@@ -17,11 +23,18 @@ public class PlayerHealth : MonoBehaviour {
     {
         _currentHealth = _startingHealth;
         _dead = false;
+
+        // Update the health slider's value and color.
+        SetHealthUI();
     }
 
     public void TakeDamage(float amount)
     {
         _currentHealth -= amount;
+
+        // Change the UI elements appropriately.
+        SetHealthUI ();
+
         if (_currentHealth <= 0f && !_dead)
         {
             OnDeath();
@@ -38,5 +51,14 @@ public class PlayerHealth : MonoBehaviour {
     public float GetHealth()
     {
         return _currentHealth;
+    }
+
+    private void SetHealthUI ()
+    {
+        // Set the slider's value appropriately.
+        _slider.value = _currentHealth;
+
+        // Interpolate the color of the bar between the choosen colours based on the current percentage of the starting health.
+        _fillImage.color = Color.Lerp (_zeroHealthColor, _fullHealthColor, _currentHealth / _startingHealth);
     }
 }
