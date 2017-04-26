8e20411f432e76132eb28e57bfefa2429152e745
Pedro Boueke <pedro.boueke@bigdatacorp.com.br>
2017-04-26 18:58:37 -0300

    Added XP GUI

--- Assets/Scripts/Player/PlayerLevel.cs
+++ Assets/Scripts/Player/PlayerLevel.cs
@@ -1,16 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerLevel : MonoBehaviour {
 
-    public float _levelMultiplier = 1;
     public int _xp;
+
+    public Text playerLevelDisplay;
+    public Slider playerLevelBar;
 
     // Use this for initialization
     void Start () {
         _xp = 0;
-	}
+
+        // starting lvl: 1
+        addXp(1);
+    }
 
     /// <summary>
     /// Adds experience to the player`s leveling system.
@@ -18,16 +24,33 @@ public class PlayerLevel : MonoBehaviour {
     /// <param name="experience"> The amount of e_xperience to be added.</param>
     public void addXp(int experience)
     {
+        int initialLevel = getLevel();
         _xp += experience;
+        // update UI
+        int currentLevel = getLevel();
+        int xpForCurrentLevel = getXpForLevel(currentLevel);
+        int xpForNextLevel = getXpForLevel(currentLevel + 1);
+        playerLevelDisplay.text = currentLevel.ToString();
+        playerLevelBar.value = (float)(_xp - xpForCurrentLevel) / (float)(xpForNextLevel - xpForCurrentLevel);
     }
 
     /// <summary>
     /// Gets the player level based on the current _xp. 
-    /// This version simply uses the SQRT aproach, it may be revisited in the future.
+    /// This version simply uses the SQRT aproach, it may be revisited in the future (if so, getXpForLevel also needs an update).
     /// </summary>
     /// <returns>The current level of the player.</returns>
     private int getLevel()
     {
-        return (int)(Mathf.Floor(_levelMultiplier * Mathf.Sqrt(_xp)));
+        return (int)(Mathf.Floor(Mathf.Sqrt(_xp)));
+    }
+
+    /// <summary>
+    /// Does the opposite of GetLevel(), returning the amount of XP needed for the targeted level.
+    /// </summary>
+    /// <param name="level"></param>
+    /// <returns></returns>
+    private int getXpForLevel (int level)
+    {
+        return (int)Mathf.Pow(level, 2f);
     }
 }
