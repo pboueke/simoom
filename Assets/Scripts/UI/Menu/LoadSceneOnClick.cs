198c85a2f50bfce167f3603f7049b9c614bb2ed6
Pedro Boueke <phboueke@gmail.com>
2017-06-22 22:25:49 -0300

    [BugFix] Removed duplicated objects at game restart.

--- Assets/Scripts/UI/Menu/LoadSceneOnClick.cs
+++ Assets/Scripts/UI/Menu/LoadSceneOnClick.cs
@@ -4,8 +4,25 @@ using UnityEngine.SceneManagement;
 
 public class LoadSceneOnClick : MonoBehaviour {
 
+	public string playerObjectName = "Carpet";
+
+	private GameObject player;
+
+	void Start() {
+		player = GameObject.Find (playerObjectName);
+	}
+
 	public void LoadByIndex(int sceneIndex)
 	{
 		SceneManager.LoadScene (sceneIndex);
+	}
+
+	public void ForceLoadByIndex(int sceneIndex)
+	{
+		Destroy (player.gameObject);
+		Destroy (GameObject.Find ("Canvas").gameObject);
+		Destroy (GameObject.Find ("Main Camera").gameObject);
+
+		LoadByIndex (sceneIndex);
 	}
 }
